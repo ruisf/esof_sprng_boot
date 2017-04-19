50768f3527c078679b0618d6da489a7f02565b0f
ruisf <rui@ruiprogramador.net>
2017-04-19 23:45:10 +0100

    remove warnings

--- src/main/java/pt/ulisboa/tecnico/softeng/bank/exception/BankException.java
+++ src/main/java/pt/ulisboa/tecnico/softeng/bank/exception/BankException.java
@@ -2,4 +2,9 @@ package pt.ulisboa.tecnico.softeng.bank.exception;
 
 public class BankException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
